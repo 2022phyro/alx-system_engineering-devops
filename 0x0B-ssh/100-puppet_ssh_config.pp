784ddb675a56d7867cdf8e4e7447d30161cd1a5e
2022phyro <phyrokelstein2@gmail.com>
2023-03-26 10:02:12 -0700

    Changed mode for file with no 1 and created puppet manifests for configuring my local machine

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,12 @@
 # Configures my client ssh configuration
-exec {"main":
-	path "/usr/bin/env; /bin"
-	command 'echo "    PassswordAuthentication no\n    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
-	return [0,1]
+exec {'Turn off passwd':
+  path    => '/usr/bin/',
+  command => 'echo "    PassswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns  => ['0', '1'],
+}
+
+exec {'Declare identity file':
+  path    => '/usr/bin/',
+  command => 'echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  returns  => ['0', '1'],
 }
