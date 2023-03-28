d63fd6839041cb15ec0a947085abd9aae4a24d7e
2022phyro <phyrokelstein2@gmail.com>
2023-03-28 12:11:43 +0100

    Debugging the manifest, compressed everything to one exec call

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,5 @@
 #This manifest configures an nginx server
-exec {'update':
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx',
+exec {'install_and_configure_nginx':
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World! | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/2022phyro permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
   provider => shell,
 }
-exec {'welcome page':
-  command   => 'echo "Hello World!" > /var/www/html/index.nginx-debian.html',
-  provider => shell,
-}
-exec {'redirection':
-  command => 'sudo sed -i "s/server_name _;/server_name;\n\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/www.youtube.com;\n\t}/g" /etc/nginx/sites-enabled/default',
-  provider => shell,
-}
-exec {'restart':
-  command  => 'sudo service nginx restart',
-  provider => shell,
-}
-
