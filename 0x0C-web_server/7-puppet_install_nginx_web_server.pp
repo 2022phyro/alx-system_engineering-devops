14270b70f8855a83275816b082a2158bd9514b5f
2022phyro <phyrokelstein2@gmail.com>
2023-03-28 04:21:03 -0700

    Still trying to find the error, completed an unescaped string literal

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 #This manifest configures an nginx server
 exec {'install_and_configure_nginx':
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World! | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/2022phyro permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/2022phyro permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
   provider => shell,
 }
