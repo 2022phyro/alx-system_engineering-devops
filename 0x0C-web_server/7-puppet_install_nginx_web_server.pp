2ea738dd9f389c9357f1d37ac61dabf4b2215c17
2022phyro <phyrokelstein2@gmail.com>
2023-03-28 01:13:57 -0700

    Fixed syntax errors in the puppet file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,30 @@
 package {'nginx':
   ensure   => present,
-  name     => "nginx",
+  name     => nginx,
   provider => apt-get,
 }
 package {'nginx-extras':
   ensure   => present,
-  name     => "nginx",
+  name     => nginx,
   provider => apt-get,
 }
 file {'welcome page':
   ensure  => present,
-  path    => /var/www/html/index.nginx-debian.html,
-  mode    => 600,
-  content => "Hello World!",
+  path    => '/var/www/html/index.nginx-debian.html',
+  mode    => '0600',
+  content => 'Hello World!',
 }
 file {'Error page':
   ensure  => present,
-  path    => /var/www/html/error404.html,
-  mode    => 600,
-  content => "Ceci n'est pas une page",
+  path    => '/var/www/html/error404.html',
+  mode    => '0600',
+  content => 'Ceci n\'est pas une page',
 }
-exec {'configure redirection':
+exec {'redirection':
   command => "sed -i '/# pass PHP scripts to FastCGI server/i \\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n' /etc/nginx/sites-enabled/default",
   path    => /usr/bin,
 }
-exec {'configure 404 error page':
+exec {'404_error page':
   command => "sed -i '/\tlocation \/redirect_me {/i \\terror_page 404 /error404.html;\n' /etc/nginx/sites-enabled/default",
   path    => /usr/bin,
 }
