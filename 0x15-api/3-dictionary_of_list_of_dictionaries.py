f82c80d8a2fed235ee45cf97edde7255a24773c3
2022phyro <phyrokelstein2@gmail.com>
2023-04-26 12:45:17 +0100

    No 3. testing. first requested for the user's api then in a loop continued requesting althouhg i kno this  is probably bad practice

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 
-"""This uses the provided api to print stuff
-Here we use json api to print out info about a specific employee
+"""This uses the provided api to get info about users
+Here we use json api to get out info about all users
 Then we save this information to a file in json format"""
 
-import csv
 import json
 import requests
-import sys
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
-    name = requests.get("{}users/{}".format(url, sys.argv[1])).json()
-    u_name = name.get('username')
-    todo = requests.get("{}todos?userId={}".format(url, sys.argv[1])).json()
-    end_dict = {}
-    list_ = []
-    for val in todo:
-        end_dict.update({"task": val.get('title')})
-        end_dict.update({"completed": val.get('completed')})
-        end_dict.update({"username": u_name})
-        list_.append(end_dict)
-    with open("{}.json".format(sys.argv[1]), 'w') as fp:
-        json.dump({sys.argv[1]: list_}, fp)
+    users = requests.get("{}users".format(url)).json()
+    end = {}
+    for user in users:
+        i = user.get('id')
+        todo = requests.get("{}todos?userId={}".format(url, i)).json()
+        list_ = []
+        for val in todo:
+            end_dict = {}
+            end_dict.update({"username": user.get('username')})
+            end_dict.update({"task": val.get('title')})
+            end_dict.update({"completed": val.get('completed')})
+            list_.append(end_dict)
+        end.update({i: list_})
+    with open("todo_all_employees.json", 'w') as fp:
+        json.dump(end, fp)
