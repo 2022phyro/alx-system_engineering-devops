425051a16eb717d1d4fd08d0ca343a7f4b32bb18
2022phyro <phyrokelstein2@gmail.com>
2023-04-26 11:15:18 +0100

    No 0 used json practice ap

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This uses the provided api to print stuff"""
+import requests
+import sys
+url = "https://jsonplaceholder.typicode.com/"
+name = requests.get("{}users/{}".format(url, sys.argv[1])).json()['name']
+todo = requests.get("{}todos?userId={}".format(url, sys.argv[1])).json()
+print("Employee {} is done with".format(name), end=" ")
+completed = 0
+i = 0
+for val in todo:
+    if val['completed']:
+        completed += 1
+    i += 1
+print("tasks({}/{})".format(completed, i))
+for val in todo:
+    if val['completed']:
+        print("\t {}".format(val['title']))
