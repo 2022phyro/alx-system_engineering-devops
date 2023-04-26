b545d94832472e59e689b4647ef976a8b33fdcce
2022phyro <phyrokelstein2@gmail.com>
2023-04-26 11:30:35 +0100

    Made the code to only be executed when not imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,17 +6,19 @@ Here we use json api to print out a specific employee"""
 
 import requests
 import sys
-url = "https://jsonplaceholder.typicode.com/"
-name = requests.get("{}users/{}".format(url, sys.argv[1])).json()['name']
-todo = requests.get("{}todos?userId={}".format(url, sys.argv[1])).json()
-print("Employee {} is done with".format(name), end=" ")
-completed = 0
-i = 0
-for val in todo:
-    if val['completed']:
-        completed += 1
-    i += 1
-print("tasks({}/{})".format(completed, i))
-for val in todo:
-    if val['completed']:
-        print("\t {}".format(val['title']))
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    name = requests.get("{}users/{}".format(url,
+                                            sys.argv[1])).json().get('name')
+    todo = requests.get("{}todos?userId={}".format(url, sys.argv[1])).json()
+    print("Employee {} is done with".format(name), end=" ")
+    completed = 0
+    i = 0
+    for val in todo:
+        if val.get('completed'):
+            completed += 1
+        i += 1
+    print("tasks({}/{})".format(completed, i))
+    for val in todo:
+        if val.get('completed'):
+            print("\t {}".format(val.get('title')))
