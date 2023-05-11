28e923ba7ed800ff5c4e230a350b3f1bd7b7620e
2022phyro <phyrokelstein2@gmail.com>
2023-05-11 21:56:02 +0100

    Switched to a new organization and took advantage of the alx student pack

--- 0x18-webstack_monitoring/example.py
+++ 0x18-webstack_monitoring/example.py
@@ -1,5 +1,5 @@
 """
-Get the total number of active hosts returns "OK" response
+Get all hosts for your organization returns "OK" response
 """
 
 from datadog_api_client import ApiClient, Configuration
@@ -8,6 +8,8 @@ from datadog_api_client.v1.api.hosts_api import HostsApi
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
     api_instance = HostsApi(api_client)
-    response = api_instance.get_host_totals()
+    response = api_instance.list_hosts(
+        filter="env:ci",
+    )
 
     print(response)
